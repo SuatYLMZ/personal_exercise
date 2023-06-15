7923cc185e56a8e8c7c8cc3cc2c5695648696a46
SuatYLMZ <furkansuatylmz@gmail.com>
2023-06-14 21:02:50 -0700

    intializing spring boot

---
+++ src/test/java/com/suatylmz/personalexercise/PersonalExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suatylmz.personalexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
