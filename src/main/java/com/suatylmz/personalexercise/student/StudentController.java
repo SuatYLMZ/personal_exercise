0d9b774a98d8efe3ea48c53ea1f92576b9590663
SuatYLMZ <furkansuatylmz@gmail.com>
2023-06-14 21:27:31 -0700

    starting the project

---
+++ src/main/java/com/suatylmz/personalexercise/student/StudentController.java
@@ -0,0 +1,22 @@
+package com.suatylmz.personalexercise.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<String> findAllStudents(){
+        return List.of(
+           "Suat",
+           "Hello World"
+        );
+    }
+
+
+}
